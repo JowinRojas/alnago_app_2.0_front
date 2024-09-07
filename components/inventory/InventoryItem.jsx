6d00392dadafa43c0b58a0b7a48a73700081d1c3
jowin rojas <jowinx-001@hotmail.com>
2024-09-07 10:37:08 -0500

    ajustes proyecto

--- components/inventory/InventoryItem.jsx
+++ components/inventory/InventoryItem.jsx
@@ -53,7 +53,7 @@ const InventoryItem = ({ name, status, fotos, videos, detalles }) => {
           </View>
 
           <TextInput
-            className="w-full m-2 border-2 rounded-2xl px-2 text-2xl"
+            className="w-full border-2 rounded-2xl px-2 py-1 text-2xl"
             placeholder="Detalles"
             editable
             multiline
