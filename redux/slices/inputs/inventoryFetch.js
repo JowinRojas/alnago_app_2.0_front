5c9516e18bda8edf2553bcc757b025b297d2ceda
dev_alnago <dev@alnago.com>
2024-09-19 11:08:08 -0500

    prueba de preview

--- redux/slices/inputs/inventoryFetch.js
+++ redux/slices/inputs/inventoryFetch.js
@@ -1,28 +1,26 @@
 import { Alert } from "react-native";
 import { URLbase } from "../../../config";
 
-export const sendInv = async ( {fotos, comentarios, direccionInventario} ) => {
-    
-    const data = new FormData();
-    fotos.map( foto => data.append('fotos', {uri:foto, name:'photito.jpg', type:'image/jpeg'}))
-    data.append('comentarios', comentarios);
-    data.append('direccion', direccionInventario);
-    console.log("data", data);
-    
-    try {
-      const response = await fetch(`${URLbase}/google/images`,{
-          method: 'POST',
-          body: JSON.stringify({data}),
-          credentials: 'include',
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          }    
-      )
-      if(response) return;
+export const sendInv = async ({ fotos, comentarios, direccionInventario }) => {
+  const data = new FormData();
+  fotos.map((foto) =>
+    data.append("fotos", { uri: foto, name: "photito.jpg", type: "image/jpeg" })
+  );
+  data.append("comentarios", comentarios);
+  data.append("direccion", direccionInventario);
+  console.log("data", data);
 
-    } catch(error) {
-      console.log(error)
-    }
-
-}
+  try {
+    const response = await fetch(`${URLbase}/google/images`, {
+      method: "POST",
+      body: data,
+      credentials: "include",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    if (response) return;
+  } catch (error) {
+    console.log(error);
+  }
+};
