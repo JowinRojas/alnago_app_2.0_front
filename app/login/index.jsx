e5ebfcec52caf1ac0829875e69404c629bdacbef
jowin rojas <jowinx-001@hotmail.com>
2024-10-10 11:44:29 -0500

    simbolo de carga en login

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,5 +1,13 @@
 import { Link, Stack, useRouter } from "expo-router";
-import { Alert, Image, Pressable, Text, TextInput, View } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SingInIcon } from "../../components/Icons";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -12,22 +20,30 @@ import { styled } from "nativewind";
 const StyledPressable = styled(Pressable);
 
 export default function Login() {
-  
   const router = useRouter();
   const status = useSelector((state) => state.loginStatus.status);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    
     dispatch(checkLogin());
-
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
     if (status == "login") {
+      setLoading(true);
       router.push("inventory");
     }
   }, [status]);
 
   const loginForm = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    },2000);
     if (email == "" || password == "") {
       Alert.alert("Todos los campos son obligatorios");
       return;
@@ -72,17 +88,17 @@ export default function Login() {
         </View>
 
         <View className="m-5">
-
-
-          <StyledPressable
-            className="flex-row justify-between items-center bg-alnago-2 rounded-md px-4 active:opacity-75 active:scale-90"
-            onPress={()=>loginForm()}
-          >
-            <Text className="text-alnago-1 text-2xl p-1">Ingresar</Text>
-            <SingInIcon />
-          </StyledPressable>
-
-
+          {loading ? (
+            <ActivityIndicator size="larges" color="#000000" />
+          ) : (
+            <StyledPressable
+              className="flex-row justify-between items-center bg-alnago-2 rounded-md px-4 active:opacity-75 active:scale-90"
+              onPress={() => loginForm()}
+            >
+              <Text className="text-alnago-1 text-2xl p-1">Ingresar</Text>
+              <SingInIcon />
+            </StyledPressable>
+          )}
         </View>
       </View>
     </View>
