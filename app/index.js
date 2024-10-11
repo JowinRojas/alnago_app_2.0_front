8cdfc4898d928bf7f0f880d4291a12ad3c61f5e0
jowin rojas <jowinx-001@hotmail.com>
2024-10-11 16:43:13 -0500

    limpieza de carpetas

--- app/index.js
+++ app/index.js
@@ -1,33 +1,5 @@
-// import InventoryHome from "./inventory";
 import Login from "./login";
 
 export default function Index() {
-  // return <InventoryHome />;
   return <Login />;
 }
-
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect, useState } from "react";
-// import { checkLogin } from "../redux/slices/login/loginController";
-// import InventoryHome from "./inventory";
-// import Login from "./login";
-
-// export default function Index() {
-//   const [page, setPage] = useState(false);
-
-//   const status = useSelector((state) => state.loginStatus.status);
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(checkLogin());
-//     if (status == "login") {
-//       setPage(true);
-//     } else {
-//       setPage(false);
-//     }
-//   }, [status]);
-
-//   return (
-//     page ? <InventoryHome /> : <Login />
-//   )
-// }
